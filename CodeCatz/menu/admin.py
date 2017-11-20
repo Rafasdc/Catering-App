db437f01b21734dd0c3194bc51647df66aff99a8
kbromma <kbromma@gmail.com>
2017-11-20 12:57:03 -0800

    Updated menu

--- CodeCatz/menu/admin.py
+++ CodeCatz/menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 from django import forms
+
+from.models import Category, Ingredient, IngredientQuantity, Recipe, RecipeQuantity, MenuItem, Menu
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Ingredient)
+admin.site.register(IngredientQuantity)
+admin.site.register(RecipeQuantity)
+admin.site.register(Recipe)
+admin.site.register(MenuItem)
+admin.site.register(Menu)
--- CodeCatz/menu/models.py
+++ CodeCatz/menu/models.py
@@ -2,18 +2,21 @@ from django.db import models
 from django.utils.timezone import now
 import uuid
 from smart_selects.db_fields import ChainedForeignKey, ChainedManyToManyField
-from django.utils.translation import ugettext_lazy as _
 from django_extensions.db.models import TimeStampedModel, TitleSlugDescriptionModel
 from django_extensions.db.fields import AutoSlugField
-
+from django.utils.translation import ugettext_lazy as _
 class Category(TitleSlugDescriptionModel):
+    
+    class Meta:
+        verbose_name='Category'
+        verbose_name_plural='Categories'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 class Ingredient(TimeStampedModel):
-    name=models.CharField(_('Name'), max_length=100)
-    description=models.TextField(_('Description'), blank=True, null=True)
+    name=models.CharField('Name', max_length=100)
+    description=models.TextField('Description', blank=True, null=True)
     category=models.ForeignKey(Category)
 
     def __str__(self):
@@ -24,6 +27,10 @@ class IngredientQuantity(TimeStampedModel):
     ingredient=models.ForeignKey(Ingredient)
     quantity=models.DecimalField(_('Quantity'), max_digits=5, decimal_places=2)
     unit=models.CharField(_('Unit'), choices=UNIT_CHOICES, max_length=3, blank=True, null=True)
+
+    class Meta:
+        verbose_name=_('Ingredient Quantity')
+        verbose_name_plural=_('Ingredient Quantities')
 
     def __str__(self):
         if self.unit:
@@ -43,7 +50,7 @@ class Recipe(TitleSlugDescriptionModel, TimeStampedModel):
     (u'bfast', u'Breakfast'),
     )
     ingredient_quantities = models.ManyToManyField(IngredientQuantity, blank=True)
-    meal_type = models.CharField(_('Type'), max_length=1, choices=APPROVAL_CHOICES, null=True)
+    meal_type = models.CharField(_('Type'), max_length=100, choices=APPROVAL_CHOICES, null=True)
     cost = models.DecimalField(max_digits=6, decimal_places=2, help_text="Select cost for this menu item.", null=True)
 
     def __str__(self):
@@ -56,8 +63,24 @@ class RecipeQuantity(TimeStampedModel):
     recipe = models.ForeignKey(Recipe)
     recipe_quantities = models.IntegerField(_('Quantity'), default=1)
 
+    class Meta:
+        verbose_name=_('Recipe Quantity')
+        verbose_name_plural=_('Recipe Quantities')
+
+    def __str__(self):
+        return ' '.join([str(self.recipe_quantities), self.recipe.title])
+
+
+
 class MenuItem(TimeStampedModel):
     menu_constituents = models.ManyToManyField(RecipeQuantity)
+
+    def __str__(self):
+        stringVal = ''
+        for x in self.menu_constituents.all():
+            stringVal += x.__str__() + " "
+        return stringVal
+
 
 class Menu(TimeStampedModel, models.Model):
     """
@@ -72,3 +95,7 @@ class Menu(TimeStampedModel, models.Model):
     @property
     def total_cost(self):
         pass
+
+    def __str__(self):
+        return ' '.join(self.menu_items.all())
+        
