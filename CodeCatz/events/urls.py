75f504ceca6ee9d32e2b135776240d4194c09933
Dyadem <jasonsanche@gmail.com>
2017-11-20 21:34:55 -0800

    tinkered with event views

--- CodeCatz/events/urls.py
+++ CodeCatz/events/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
+from django.views.generic import TemplateView
 from . import views
 
 
 urlpatterns = [
-
+    url(r'^event_detail/', TemplateView.as_view(template_name="events/event_detail.html"), name='book'),
 ]
--- CodeCatz/events/views.py
+++ CodeCatz/events/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class EventDetailView(LoginRequiredMixin,generic.DetailView):
+    model = Event
+    context_object_name = 'event_detail'
+    template_name = 'events/event_detail.html'
+
+    def get_queryset(self):
+        return Event.objects.filter(user=self.request.user).filter(status__exact='o').order_by('date')
