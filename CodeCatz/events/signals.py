0609c12c7b49d0d8656cff40bac5c825c5f29126
kbromma <kbromma@gmail.com>
2017-11-28 23:09:03 -0800

    Scheduler updates and sends messages based on time

--- CodeCatz/events/signals.py
+++ CodeCatz/events/signals.py
@@ -3,10 +3,17 @@ from django.core.mail import send_mail
 from django.dispatch import receiver
 from .models import Event
 from django.conf import settings
+import datetime
 
 @receiver(signals.post_save, sender=Event)
 def event_post_save(sender, instance, signal, *args, **kwargs):
 	
 	if instance.status == "p":    
-		send_mail('Pending event request (CodeCatz)', "There is a pending event from {:s} {:s}. Please confirm the details on the management tab.".format(instance.user.first_name, instance.user.last_name), "automated@code.catz.ca", [settings.EMAIL_HOST_USER,], fail_silently=False)
-
+		#send to manager
+		send_mail('Event {:d} is pending.'.format(instance.id), "There is a pending event from {:s} {:s}. Please confirm the details on the management tab.".format(instance.user.first_name, instance.user.last_name), "automated@code.catz.ca", [settings.EMAIL_HOST_USER,], fail_silently=False)
+	if instance.status == "a":
+    	#send to user
+		endDate = instance.getEndDate
+		endTime = instance.endTime
+		compare = datetime.datetime.combine(endDate, endTime)
+		send_mail('Event Approved!', "Hi {:s} {:s}, \n\n This is to notify you that your event on {:%B %d, %Y} has been approved. We will email you one week beforehand to confirm. \n\n Thanks,\nCats Catering".format(instance.user.first_name, instance.user.last_name, compare), "automated@code.catz.ca", [instance.user.email], fail_silently=False)
--- CodeCatz/scheduler/apps.py
+++ CodeCatz/scheduler/apps.py
@@ -1,5 +1,43 @@
 from django.apps import AppConfig
+import datetime
+from django.db.models import signals
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from django.conf import settings
 
 
 class SchedulerConfig(AppConfig):
     name = 'scheduler'
+    verbose_name = "Scheduler"
+    def ready(self):
+        from events.models import Event
+
+        events = Event.objects.all()
+        for event in events:
+            endDate = event.getEndDate
+            endTime = event.endTime
+            compareEnd = datetime.datetime.combine(endDate, endTime)
+            date = event.date
+            startTime = event.startTime
+            compareStart = datetime.datetime.combine(date, startTime)
+            now = datetime.datetime.now()
+        
+            if(compareEnd < now):
+                if event.status != 'c':
+                    user = event.user
+                    userEmail = user.email
+                    send_mail('Event Over, Please Delete', "Event {:d} from {:%B %d, %Y} by user {:s} {:s} is over. Please confirm payment and delete.".format(event.id, compareEnd, user.first_name, user.last_name), "automated@code.catz.ca", [settings.EMAIL_HOST_USER,], fail_silently=False)
+                    event.status = 'c'
+                    event.save()
+
+            elif(now > (compareStart-datetime.timedelta(weeks=1))):
+                user = event.user
+                if(event.status == 'p'):
+                    send_mail('One week before event {:d}, still pending'.format(event.id), "There is a pending event from {:s} {:s} and their event starts on {:%B %d, %Y}. Please confirm the details on the management tab.".format(user.first_name, user.last_name, compareStart), "automated@code.catz.ca", [settings.EMAIL_HOST_USER,], fail_silently=False)
+                elif(event.status == 'a'):
+                    #For manager
+                    send_mail('One week before event {:d}'.format(event.id), "There is an event from {:s} {:s} and their event starts on {:%B %d, %Y}. Please confirm employees and inventory for the event.".format(user.first_name, user.last_name, compareStart), "automated@code.catz.ca", [settings.EMAIL_HOST_USER,], fail_silently=False)
+                    #For user
+                    send_mail('One Week Before Your Event!', "Hi {:s} {:s},\n\n This is to notify you that your event on {:%B %d, %Y} is well on its way! Please email back to confirm.\n\nWe will see you soon,\n Cats Catering".format(user.first_name, user.last_name,compareStart), "automated@code.catz.ca", [user.email], fail_silently=False)
+                    event.status = 'n'
+                    event.save()
