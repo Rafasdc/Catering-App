524b9e9b7a8a7c7626e7c8f1192b96c4f6e65af5
Rafael Solorzano <rafa@solorzanoc.com>
2017-11-30 21:10:30 -0800

    remove comments

--- CodeCatz/management/test_models.py
+++ CodeCatz/management/test_models.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-#from django.auth import User
 from .models import Employee
 from events.models import Event
 from register.models import *
@@ -109,33 +108,3 @@ class EmployeeModelTest(TestCase):
         employee = Employee.objects.get(id=1)
         event = employee.event.first().id
         self.assertEquals(event,1)
-
-
-
-
-"""
-    def test_first_name_label(self):
-        author=Author.objects.get(id=1)
-        field_label = author._meta.get_field('first_name').verbose_name
-        self.assertEquals(field_label,'first name')
-
-    def test_date_of_death_label(self):
-        author=Author.objects.get(id=1)
-        field_label = author._meta.get_field('date_of_death').verbose_name
-        self.assertEquals(field_label,'died')
-
-    def test_first_name_max_length(self):
-        author=Author.objects.get(id=1)
-        max_length = author._meta.get_field('first_name').max_length
-        self.assertEquals(max_length,100)
-
-    def test_object_name_is_last_name_comma_first_name(self):
-        author=Author.objects.get(id=1)
-        expected_object_name = '%s, %s' % (author.last_name, author.first_name)
-        self.assertEquals(expected_object_name,str(author))
-
-    def test_get_absolute_url(self):
-        author=Author.objects.get(id=1)
-        #This will also fail if the urlconf is not defined.
-        self.assertEquals(author.get_absolute_url(),'/catalog/author/1')
-"""
