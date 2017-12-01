5ccfce76c6ea88d7f79cb01ccfca3d8a25122c82
Rafael Solorzano <rafa@solorzanoc.com>
2017-11-30 20:04:19 -0800

    management forms tests

--- CodeCatz/management/test_forms.py
+++ CodeCatz/management/test_forms.py
@@ -3,12 +3,75 @@ from django.test import TestCase
 import datetime
 from django.utils import timezone
 from .forms import *
+from events.models import Event
 
 class EmployeeFormTest(TestCase):
 
     def test_employee_form_wage_field(self):
         form = EmployeeForm()
         self.assertTrue(form.fields['wage_hour'].label == None or form.fields['wage_hour'].label == 'Wage hour')
+
+class AssignEmployeeEventFormTest(TestCase):
+    
+    def setUpTestData():
+        #Set up non-modified objects used by all test methods
+        test_user = User()
+        test_user.first_name = 'Bob'
+        test_user.last_name = 'Test'
+        test_user.email = 'bob@test.com'
+        test_user.save()
+        test_profile = UserProfile.objects.get(id=test_user.id)
+        test_profile.phone='222-222-2222'
+        test_profile.save()
+        test_event_1 = Event(user=test_user)
+        test_event_1.numGuests = 200
+        test_event_1.save()
+        test_event_2 = Event(user=test_user)
+        test_event_2.numGuests = 200
+        test_event_2.save()
+        test_employee = Employee.objects.create(profile=test_profile)
+        test_employee.event = (test_event_1,test_event_2)
+        test_employee.save()
+    
+
+    def test_assign_employee_event_form_event_field(self):
+        form = AssignEmployeeEvent()
+        self.assertTrue(form.fields['event'].label == None or form.fields['wage_hour'].label == 'Event')
+    
+    #Overlap on same date and same start time
+    def test_assign_employee_event_form_event_overlap_full(self):
+        test_event_1 = Event.objects.get(id=1)
+        test_event_2 = Event.objects.get(id=2)
+        event = (test_event_1, test_event_2)
+        form_data = {'event':event}
+        form = AssignEmployeeEvent(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    #Overlap on running time (same date, but different start times)
+    def test_assign_employee_event_form_event_overlap_hours(self):
+        test_event_1 = Event.objects.get(id=1)
+        test_event_1.startTime = datetime.time(12,00)
+        test_event_1.save()
+        test_event_2 = Event.objects.get(id=2)
+        event = (test_event_1, test_event_2)
+        form_data = {'event':event}
+        form = AssignEmployeeEvent(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    #No overlaping between events
+    def test_assign_employee_event_form_event_no_overlap(self):
+        test_event_1 = Event.objects.get(id=1)
+        test_user = User.objects.get(id=1)
+        test_event_1.date = test_event_1.date + datetime.timedelta(days=7)
+        test_event_1.save()
+        print(test_event_1.date)
+        test_event_2 = Event.objects.get(id=2)
+        event = (test_event_1, test_event_2)
+        form_data = {'event':event}
+        form = AssignEmployeeEvent(data=form_data)
+        self.assertTrue(form.is_valid())
+    
+
 
     """
     def test_renew_form_date_field_label(self):
